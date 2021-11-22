a159bab479afb882588cff2e2866ad64f5fb1842
majo1569 <64664464+majo1569@users.noreply.github.com>
2021-11-22 10:24:55 -0300

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "server1" {
+resource "aws_instance" "grupo-11" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.subnet1.id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_icmp_ssh.id, aws_security_group.allow_http_https.id]
-  private_ip                  = var.subnet1_private_ip
+  private_ip                  = 172.16.11.4/28
   key_name                    = aws_key_pair.key1.key_name
   user_data                   = file("user-data.sh")
 
   tags = {
-    Name        = "server-1"
-    Owner       = var.username
+    Name        = "grupo-11"
+    Owner       = "student_11"
     Environment = "develop"
     OS          = "amazon-linux"
   }
