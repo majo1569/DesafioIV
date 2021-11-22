c6635eb1d183d5e4df250052640d3de948f8a4ec
majo1569 <64664464+majo1569@users.noreply.github.com>
2021-11-22 20:59:31 -0300

    Update vpc_security_group for allow_http_ssh.id

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "grupo-11" {
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.subnet1.id
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.allow_icmp_ssh.id, aws_security_group.allow_http_https.id]
+  vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
   private_ip                  = "172.16.11.4"
   key_name                    = var.key1
   user_data                   = file("user-data.sh")
