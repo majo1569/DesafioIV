54101cbe568bb70ba9032c643df67b831d05d600
majo1569 <64664464+majo1569@users.noreply.github.com>
2021-11-23 08:22:45 -0300

    Delete resources vpc , internet gateway | Update vpc id

--- networking.tf
+++ networking.tf
@@ -1,13 +1,6 @@
-resource "aws_vpc" "main1" {
-  cidr_block           = "172.16.11.0/28"
-  enable_dns_hostnames = true
-  tags = {
-    "Name" = "Main 1 VPC"
-  }
-}
 
 resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.main1.id
+  vpc_id                  = aws_vpc.vpc1_id
   cidr_block              = "172.16.11.0/28" //var.subnet1_cidr
   map_public_ip_on_launch = true
 
@@ -17,16 +10,10 @@ resource "aws_subnet" "subnet1" {
     "Name" = "grupo-11"
   }
 }
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main1.id
 
-  tags = {
-    "Name" = "Gateway Main 1"
-  }
-}
 
 resource "aws_route_table" "r" {
-  vpc_id = aws_vpc.main1.id
+  vpc_id = aws_vpc.vpc1_id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
