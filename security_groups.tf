3c18ba29ec5e6d749d7d0d87495ea5f041a3715d
majo1569 <64664464+majo1569@users.noreply.github.com>
2021-11-22 21:04:21 -0300

    Update security group allow http and ssh

--- security_groups.tf
+++ security_groups.tf
@@ -1,7 +1,7 @@
-resource "aws_security_group" "allow_icmp_ssh" {
-  name        = "allow_icmp_ssh"
-  description = "Permite ICMP y SSH"
-  vpc_id      = aws_vpc.main1.id
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Permite HTTP y SSH"
+  vpc_id      = var.vpc1_id
 
   ingress {
     description = "SSH from Anywere"
@@ -11,33 +11,7 @@ resource "aws_security_group" "allow_icmp_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "ICMP from Anywere"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "Ping y SSH"
-  }
-}
-
-resource "aws_security_group" "allow_http_https" {
-  name        = "allow_http_https"
-  description = "Permite HTTP y HTTPS"
-  vpc_id      = aws_vpc.main1.id
-
-  ingress {
+ingress {
     description = "HTTP from Anywere"
     from_port   = 80
     to_port     = 80
@@ -45,14 +19,6 @@ resource "aws_security_group" "allow_http_https" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "HTTPS from Anywere"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port        = 0
     to_port          = 0
@@ -62,6 +28,7 @@ resource "aws_security_group" "allow_http_https" {
   }
 
   tags = {
-    Name = "HTTP y HTTPS"
+    Name = "HTTP y SSH"
   }
 }
+
